1021049af04c1859d83c74b18a1305774b31cd66
Luke Wilson <lukewilson.dev@gmail.com>
2025-05-17 13:36:36 -0500

    Fix the image

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://luke.frop.prof',
-	base: 'site',
 	integrations: [sitemap()],
 });
